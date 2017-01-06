278181770a032c01d6807371bf4b853aa2e8bf5b
Peter Werner <wpw.peter@gmail.com>
2017-01-06 16:15:13 +0100

    Minimal image
    Changed assure emulator awake script

--- minimal/assure_emulator_awake.sh
+++ minimal/assure_emulator_awake.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-exec $@ | grep -q ":app:connectedDebugAndroidTest"
-if [ $? == 1 ]; then
-    echo "Pattern not found!"
-else
-    adb shell input keyevent 82
-fi
+#!/usr/bin/expect -f
+
+set timeout 1800
+set cmd [lindex $argv 0]
+
+spawn {*}$cmd
+expect {
+  ":app:connectedDebugAndroidTest" {
+        spawn adb shell input keyevent 82
+        exp_continue
+  }
+  eof
+}
