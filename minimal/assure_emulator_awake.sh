9a4ee26dc4b04b4f8de23377436a594bcf2d2acc
Peter Werner <wpw.peter@gmail.com>
2017-01-06 13:46:36 +0100

    Minimal image
    Changed assure emulator awake script

--- minimal/assure_emulator_awake.sh
+++ minimal/assure_emulator_awake.sh
@@ -6,7 +6,7 @@ set cmd [lindex $argv 0]
 spawn {*}$cmd
 expect {
   ":app:connectedDebugAndroidTest" {
-        exec adb shell input keyevent 82
+        exec bash adb shell input keyevent 82
         exp_continue
   }
   eof
